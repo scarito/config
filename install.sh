747c8a8abd2bdd7df6946fe98ea93f98086b8429
scarito <ms-github@mit.edu>
2015-05-22 11:55:50 -0700

    Added non-homedir files to install script.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-FILES=(
+FILES_HOMEDIR=(
     ".bashrc"
     ".emacs"
     ".gitconfig"
@@ -10,32 +10,45 @@ FILES=(
     ".i3status.conf"
 )
 
+FILES_SRC[0]="submodule/rainbow-delimiters/rainbow-delimiters.el"
+FILES_DST[0]="$HOME/.emacs.d/rainbow-delimiters.el"
+FILES_SRC[1]="submodule/Fill-Column-Indicator/fill-column-indicator.el"
+FILES_DST[1]="$HOME/.emacs.d/fill-column-indicator.el"
+
 BACKUP_DIR="$DIR/backup"
+
+function backup_file() {
+  local src=$1
+  if [ -h $src ]
+  then
+    printf "  - Removing link %s\n" $src
+    rm $src
+  elif [ -e $src ]
+  then
+    printf "  - Moving %s to %s\n" $src $BACKUP_DIR
+    mv $src $BACKUP_DIR
+  fi
+}
+
+function link_file {
+  local src=$1
+  local link=$2
+  printf "  - Linking $link to $src\n"
+  ln -s $src $link
+}
 
 [ ! -r $BACKUP_DIR ] && mkdir $BACKUP_DIR
 
-echo "Backing up original configuration files:"
-for f in ${FILES[@]}; do
-  src="$HOME/$f"
-  if [ -r $src ]
-  then
-    if [ ! -h $src ]
-    then
-      printf "  - Moving %s to %s\n" $src $BACKUP_DIR
-      mv $src $BACKUP_DIR
-    else
-      printf "  - Removing link %s\n" $src
-      rm $src
-    fi
-  fi
+echo "Installing configuration files:"
+for f in ${FILES_HOMEDIR[@]}; do
+  backup_file "$HOME/$f"
+  link_file "$DIR/$f" "$HOME/$f"
 done
-
-echo "Linking new configuration files:"
-for f in ${FILES[@]}; do
-  src="$DIR/$f"
-  link="$HOME/$f"
-  printf "  - Linking $link to $src\n"
-  ln -s $src $link
+for (( i=0; i<${#FILES_SRC[@]}; i++ )); do
+  src="${DIR}/${FILES_SRC[$i]}"
+  dst="${FILES_DST[$i]}"
+  backup_file "$dst"
+  link_file "$src" "$dst"
 done
 
 echo "Installing gnome-terminal solarized theme"
@@ -43,6 +56,3 @@ $DIR/submodule/gnome-terminal-colors-solarized/install.sh -s dark -p Default
 
 echo "Installing dircolors solarized theme"
 echo "eval \`dircolors $DIR/submodule/dircolors-solarized/dircolors.256dark\`" > "$HOME/.dircolors"
-
-echo "Installing emacs rainbow-delimiters"
-ln -s $DIR/submodule/rainbow-delimiters/rainbow-delimiters.el $HOME/.emacs.d/rainbow-delimiters.el
